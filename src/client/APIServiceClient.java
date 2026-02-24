2ae2513dc40ed573b7d3f5030b2db480134b9a8a
daniel.palacios-m <daniel.palacios-m@mail.escuelaing.edu.co>
2026-02-24 16:18:01 -0500

    feat(): se agrega base de api

---
+++ src/client/APIServiceClient.java
@@ -0,0 +1,38 @@
+package client;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class APIServiceClient {
+    private static HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10)).build();
+
+    public String getInfo(){
+        String url = "http://localhost:8080/api/info";
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+        try {
+            HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args){
+        for (int i = 0; i<100; i++){
+            Thread thread = new Thread(() -> {
+                APIServiceClient client = new APIServiceClient();
+                System.out.println(client.getInfo());
+            });
+            thread.start();
+        }
+    }
+}
---
+++ src/main/java/com/example/Lab_0_PPCA/Lab0PpcaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab_0_PPCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0PpcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0PpcaApplication.class, args);
+	}
+
+}
